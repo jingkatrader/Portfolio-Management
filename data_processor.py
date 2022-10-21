cec1de2fe59491b4bfe70f8469ad060cd7e3bbac
jingkatrader <xu980294362@gmail.com>
2022-10-21 11:56:08 -0400

    Update codes

--- data_processor.py
+++ data_processor.py
@@ -67,6 +67,11 @@ def load_asset_price(data_path):
             asset_price = pd.read_pickle(os.path.join(data_path, asset))
             asset_prices = pd.concat([asset_prices, asset_price['4. close'].rename(asset.split('.')[0])], axis=1)
     asset_prices.sort_index(ascending=True, inplace=True)
+    asset_prices.index = pd.to_datetime(asset_prices.index)
+    try:
+        asset_prices = asset_prices.astype('float')
+    except:
+        print('Unable to convert some asset prices into float...')
     return asset_prices
 
 
@@ -79,74 +84,73 @@ def load_asset_metadata(data_path):
 
     return asset_metadata_combined
 
-# Download ETF prices
-ETFs_metadata = pd.read_excel(os.path.join(CONSTANT.INPUT_PATH, "Investable List.xlsx"))
-ETFs_ticker = ETFs_metadata.query("Country != 'CA'")['ETF_ID']
-US_ETF = load_asset_price(CONSTANT.ETF_PRICE_DIR)
+if __name__ == "__main__":
+    # Download ETF prices
+    ETFs_metadata = pd.read_excel(os.path.join(CONSTANT.INPUT_PATH, "Investable List.xlsx"))
+    ETFs_ticker = ETFs_metadata.query("Country != 'CA'")['ETF_ID']
+    US_ETF = load_asset_price(CONSTANT.ETF_PRICE_DIR)
 
-# Download US Single names from NYSE, AMEX and NASDAQ
-single_names_metadata = pd.read_excel(os.path.join(CONSTANT.INPUT_PATH, "Single names.xlsx"))
-single_names_ticker = single_names_metadata['Trading Symbol']
-download_asset_price(single_names_ticker, CONSTANT.SINGLE_NAME_DIR)
+    # Download US Single names from NYSE, AMEX and NASDAQ
+    single_names_metadata = pd.read_excel(os.path.join(CONSTANT.INPUT_PATH, "Single names.xlsx"))
+    single_names_ticker = single_names_metadata['Trading Symbol']
+    download_asset_price(single_names_ticker, CONSTANT.SINGLE_NAME_DIR)
 
-# Combine all single names from local
-US_stocks = load_asset_price(CONSTANT.SINGLE_NAME_DIR)
-US_stocks = US_stocks.astype('float')
-US_stocks.index = pd.to_datetime(US_stocks.index)
-# TODO remember to check duplicated column next time
-# Save cache for faster loading next time
-US_stocks.to_pickle("Single names prices-agg.pickle")
+    # Combine all single names from local
+    US_stocks = load_asset_price(CONSTANT.SINGLE_NAME_DIR)
+    US_stocks = US_stocks.astype('float')
+    US_stocks.index = pd.to_datetime(US_stocks.index)
+    # TODO remember to check duplicated column next time
+    # Save cache for faster loading next time
+    US_stocks.to_pickle("Single names prices-agg.pickle")
 
-US_stocks = pd.read_pickle("Single names prices-agg.pickle") # This is temp code, change to US stocks next time
-US_stocks.drop(['HVT','COHR'],axis=1,inplace=True)
+    US_stocks = pd.read_pickle("Single names prices-agg.pickle")  # This is temp code, change to US stocks next time
+    US_stocks.drop(['HVT', 'COHR'], axis=1, inplace=True)
 
-NYSE_stocks = pd.read_excel(os.path.join(CONSTANT.INPUT_PATH, "Single names-NYSE.xlsx"))['Trading Symbol'].to_list()
-download_asset_metadata(NYSE_stocks, CONSTANT.SINGLE_NAME_METADATA_DIR)
-NYSE_stocks_metadata = load_asset_metadata(CONSTANT.SINGLE_NAME_METADATA_DIR)
-NYSE_stocks_metadata.to_pickle('NYSE stocks metadata.pickle')
+    # Download all metadata for NYSE stocks
+    NYSE_stocks = pd.read_excel(os.path.join(CONSTANT.INPUT_PATH, "Single names-NYSE.xlsx"))['Trading Symbol'].to_list()
+    download_asset_metadata(NYSE_stocks, CONSTANT.SINGLE_NAME_METADATA_DIR)
+    NYSE_stocks_metadata = load_asset_metadata(CONSTANT.SINGLE_NAME_METADATA_DIR)
+    NYSE_stocks_metadata.to_pickle('NYSE stocks metadata.pickle')
 
-# download_asset_price(remove.iloc[:,0], SINGLE_NAME_DIR)
-# remove = US_stocks.columns[US_stocks.iloc[-1].isna()] #remove the stocks delisted as of last day
-# single_names_metadata[~single_names_metadata['Trading Symbol'].isin(remove)].to_excel('Single names.xlsx')
+    # download_asset_price(remove.iloc[:,0], SINGLE_NAME_DIR)
+    # remove = US_stocks.columns[US_stocks.iloc[-1].isna()] #remove the stocks delisted as of last day
+    # single_names_metadata[~single_names_metadata['Trading Symbol'].isin(remove)].to_excel('Single names.xlsx')
 
-## Remove the acquisition funds SPACs
-# single_names_metadata[~single_names_metadata['Company Name'].str.contains('ACQUISITION')].to_excel('Single names.xlsx')
+    ## Remove the acquisition funds SPACs
+    # single_names_metadata[~single_names_metadata['Company Name'].str.contains('ACQUISITION')].to_excel('Single names.xlsx')
 
+    # # ------------------------- Other API calls from Alpha Vantage   ------------------------------
+    #
+    url = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=ED&outputsize=full&apikey=1Z1KIUWET1356LLE'
+    r = requests.get(url)
+    data = r.json()
+    annual_report = pd.DataFrame(data['annualReports'])
+    quarterly_report = pd.DataFrame(data['quarterlyReports'])
+    #
+    # # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+    # url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=CAD&apikey=1Z1KIUWET1356LLE'
+    # r = requests.get(url)
+    # data = r.json()
+    # data['Realtime Currency Exchange Rate']
+    #
+    # url = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&outputsize=full&apikey=1Z1KIUWET1356LLE'
+    # r = requests.get(url)
+    # data = r.json()
+    # prices = pd.DataFrame(data['Time Series (Digital Currency Daily)'])
+    #
+    # # download TSX prices, but only available for one year
+    # url = "http://api.marketstack.com/v1/eod?access_key=18647bf3eade7aabc046ef220b476e2f&symbols=XIU.XTSE"
+    # r = requests.get(url)
+    # data = r.json()
+    # prices = pd.DataFrame(data['data'])
 
-
-# # ------------------------- Other API calls from Alpha Vantage   ------------------------------
-#
-url ='https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=ED&outputsize=full&apikey=1Z1KIUWET1356LLE'
-r = requests.get(url)
-data = r.json()
-annual_report = pd.DataFrame(data['annualReports'])
-quarterly_report = pd.DataFrame(data['quarterlyReports'])
-#
-# # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-# url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=CAD&apikey=1Z1KIUWET1356LLE'
-# r = requests.get(url)
-# data = r.json()
-# data['Realtime Currency Exchange Rate']
-#
-# url = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&outputsize=full&apikey=1Z1KIUWET1356LLE'
-# r = requests.get(url)
-# data = r.json()
-# prices = pd.DataFrame(data['Time Series (Digital Currency Daily)'])
-#
-# # download TSX prices, but only available for one year
-# url = "http://api.marketstack.com/v1/eod?access_key=18647bf3eade7aabc046ef220b476e2f&symbols=XIU.XTSE"
-# r = requests.get(url)
-# data = r.json()
-# prices = pd.DataFrame(data['data'])
-
-
-ticker = 'COHR'
-url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey=1Z1KIUWET1356LLE'
-r = requests.get(url)
-data = r.json()
-prices = pd.DataFrame(data['Time Series (Daily)'])
-prices = prices.T.sort_index(ascending=True)
-prices.index = pd.to_datetime(prices.index)
+    ticker = 'COHR'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey=1Z1KIUWET1356LLE'
+    r = requests.get(url)
+    data = r.json()
+    prices = pd.DataFrame(data['Time Series (Daily)'])
+    prices = prices.T.sort_index(ascending=True)
+    prices.index = pd.to_datetime(prices.index)
 
 
 
--- portfolio.py
+++ portfolio.py
@@ -5,31 +5,99 @@ import time
 import CONSTANT
 import factor
 import requests
+import data_processor
 
 # Test factor using only NYSE stocks (Fewer small cap stocks)
-NYSE_stocks_ret = pd.read_pickle("NYSE ret.pickle")
+
 NYSE_residuals = pd.read_pickle("NYSE residuals.pickle")
 NYSE_stocks_metadata = pd.read_pickle('NYSE stocks metadata.pickle')
 
+# Remove the stocks that have <1B Mkt Cap or only start trading after 2020
 larger_than_1B = NYSE_stocks_metadata.T[NYSE_stocks_metadata.loc['MarketCapitalization'].astype(float) > 1000000000].index
-selected_stocks = NYSE_stocks_ret.loc['2020-01-01':, larger_than_1B].dropna(axis=1).columns #remove stocks only listed post-2020
+selected_stocks = NYSE_residuals.loc['2020-01-01':, larger_than_1B].dropna(axis=1).columns #remove stocks only listed post-2020
 
-
+# find the low vol stock names
 low_vol_stocks = factor.build_low_vol_bucket(NYSE_residuals[selected_stocks].shift(22), 30, 15, [0.1])
 low_vol_stocks_names = low_vol_stocks[0.1].iloc[-1].dropna().index.to_list()
 low_vol_stocks_names.remove('Y')
 
-low_vol_stocks_ret = NYSE_stocks_ret[low_vol_stocks_names]
-low_vol_stocks_std = low_vol_stocks_ret.rolling(30, min_periods=15).std()
-low_vol_stocks_inv_vol_weight = (1/low_vol_stocks_std.iloc[-1])/(1/low_vol_stocks_std.iloc[-1]).sum()
-low_vol_stocks_cov = low_vol_stocks_ret.cov()
+# Find the highest vol stock names
+high_vol_stocks = factor.build_low_vol_bucket(NYSE_residuals[selected_stocks].shift(22), 30, 15, [0.1, 0.9])
+high_vol_stocks_names = high_vol_stocks[0.9].iloc[-1].dropna().index.to_list()
 
-# Inverse vol portfolio vol and ex-ante daily return
-low_vol_stocks_inv_vol_weight.T@low_vol_stocks_cov@low_vol_stocks_inv_vol_weight
-(1+(low_vol_stocks_ret.loc['2022-01-01':]@low_vol_stocks_inv_vol_weight)).prod()
+
+def form_inv_vol_weights(ret):
+    low_vol_stocks_std = ret.rolling(30, min_periods=15).std()
+    low_vol_stocks_inv_vol_weight = (1/low_vol_stocks_std.iloc[-1])/(1/low_vol_stocks_std.iloc[-1]).sum()
+    return low_vol_stocks_inv_vol_weight
+
+
+def report_portfolio_stats(ret, weights):
+    # Inverse vol portfolio vol and ex-ante daily return
+    cov = ret.cov()
+    portfolio_vol = weights.T@cov@weights
+    ex_ret = (1+(ret@weights)).prod() - 1
+    ex_ret_post_2022 = (1 + (ret.loc['2022-01-01':] @ weights)).prod()
+    return {'portfolio_vol' : portfolio_vol,
+            "Expected_cum_return": ex_ret,
+            'Expected_cum_return_post2022': ex_ret_post_2022
+            }
+
+
+# Download price for trading/re-balance
+data_processor.download_asset_price(low_vol_stocks_names, CONSTANT.TRADING_DIR)
+low_vol_stocks_price = data_processor.load_asset_price(CONSTANT.TRADING_DIR)
+low_vol_stocks_ret = low_vol_stocks_price.pct_change()
+
+low_vol_stocks_inv_vol_weight = form_inv_vol_weights(low_vol_stocks_ret)
+
+portfolio_summary_stats = report_portfolio_stats(low_vol_stocks_ret, low_vol_stocks_inv_vol_weight)
+
+# Export to trade book
+trading_book = pd.concat([low_vol_stocks_price.iloc[-1], low_vol_stocks_inv_vol_weight],axis=1)
+trading_book.columns = [f'Price as of {low_vol_stocks_price.index[-1].strftime("%Y-%m-%d")}', 'Weight']
+trading_book.sort_index(inplace=True)
+trading_book.to_clipboard()
+
+
+# Download price for trading/re-balance
+data_processor.download_asset_price(high_vol_stocks_names, 'Trading_high_vol')
+high_vol_stocks_price = data_processor.load_asset_price('Trading_high_vol')
+high_vol_stocks_ret = high_vol_stocks_price.pct_change()
+
+high_vol_stocks_inv_vol_weight = form_inv_vol_weights(high_vol_stocks_ret)
+
+portfolio_summary_stats = report_portfolio_stats(high_vol_stocks_ret, high_vol_stocks_inv_vol_weight)
+
+trading_book = pd.concat([high_vol_stocks_price.iloc[-1], high_vol_stocks_inv_vol_weight],axis=1)
+trading_book.columns = [f'Price as of {high_vol_stocks_price.index[-1].strftime("%Y-%m-%d")}', 'Weight']
+trading_book.sort_index(inplace=True)
+trading_book.to_clipboard()
+
+
+
+current_book = pd.read_clipboard()
+current_book['Ticker_clean'] =[x[0] for x in current_book.iloc[:,0].str.split('-')]
+current_book.groupby('Ticker_clean').sum().to_clipboard()
 
 # Equal weight portfolio vol
 low_vol_stocks_ret.mean(axis=1).std()
 
 # Show the number of stocks per sector
 NYSE_stocks_metadata[larger_than_1B].T.groupby('Sector').count()
+
+
+
+low_vol_stocks_price.iloc[-1].to_clipboard()
+
+low_vol_stocks_inv_vol_weight.to_clipboard()
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=KO&interval=5min&apikey=1Z1KIUWET1356LLE'
+r = requests.get(url)
+data = r.json()
+
+
+# Leave this code for research purpose
+NYSE_stocks_ret = pd.read_pickle("NYSE ret.pickle") # This is full NYSE Stock returns
+low_vol_stocks_ret = NYSE_stocks_ret[low_vol_stocks_names]
